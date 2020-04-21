aed1dee2260c7b6d58dcfb667c095786c12074f5
Neil Roberts <neil@sitepen.com>
2020-04-21 09:37:39 -0500

    Type casted

--- src/demo/widgets/Hello.js
+++ src/demo/widgets/Hello.js
@@ -4,7 +4,8 @@ import _WidgetBase from "dijit/_WidgetBase";
 import Button from "dijit/form/Button";
 import TextBox from "dijit/form/TextBox";
 
-export default declare([_WidgetBase], {
+/** @typedef {Partial<dijit._WidgetBase & { nameInput: dijit.form.TextBox }>} HelloState */
+export default declare([_WidgetBase], /** @type HelloState */ ({
     onChange: function (e) {
         return true;
     },
@@ -24,24 +25,24 @@ export default declare([_WidgetBase], {
         }, td);
         tr = domConstruct.create("tr", null, table);
         td = domConstruct.create("td", null, tr);
-        this.nameInput = new TextBox({
+        this.nameInput = new TextBox(/** @type Partial<dijit.form.TextBox> */ ({
             id: this.id + "Name",
             type: "text",
             name: "name",
             trim: "true",
             properCase: true
-        }).placeAt(td);
+        })).placeAt(td);
         this.own(this.nameInput);
 
         tr = domConstruct.create("tr", null, table);
         domConstruct.create("td", null, tr);
         tr = domConstruct.create("tr", null, table);
         td = domConstruct.create("td", null, tr);
-        this.own(new Button({
+        this.own(new Button(/** @type Partial<dijit.form.Button> */ ({
             label: "Greet",
             onClick: function (e) {
                 this.onChange(this.nameInput.get("value"));
             }.bind(this)
-        }).placeAt(td));
+        })).placeAt(td));
     }
-});
+}));
