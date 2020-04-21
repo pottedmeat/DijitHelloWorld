3da7067b978f412071833fb8ed776a54a32ea05d
Neil Roberts <neil@sitepen.com>
2020-04-21 10:00:12 -0500

    Fix whitespace, add screenshots

--- src/demo/index.js
+++ src/demo/index.js
@@ -2,13 +2,14 @@ import win from "dojo/_base/window";
 import dom from "dojo/dom";
 import Hello from "demo/widgets/Hello";
 import "dojo/domReady!";
-    function greet(name) {
-        dom.byId("mainHeading").innerText = "Hello " + (name || "World") + "!";
-    }
-    greet();
 
-    (new Hello({
-        onChange: function (name) {
-            greet(name);
-        }
-    })).placeAt(win.body());
+function greet(name) {
+    dom.byId("mainHeading").innerText = "Hello " + (name || "World") + "!";
+}
+greet();
+
+(new Hello({
+    onChange: function (name) {
+        greet(name);
+    }
+})).placeAt(win.body());
--- src/demo/widgets/Hello.js
+++ src/demo/widgets/Hello.js
@@ -3,45 +3,45 @@ import domConstruct from "dojo/dom-construct";
 import _WidgetBase from "dijit/_WidgetBase";
 import Button from "dijit/form/Button";
 import TextBox from "dijit/form/TextBox";
-    export default declare([_WidgetBase], {
-        onChange: function (e) {
-            return true;
-        },
-        buildRendering: function () {
-            this.domNode = domConstruct.create("div");
-            var table = domConstruct.create("table", {
-                style: {
-                    border: "1px solid #9f9f9f"
-                },
-                cellSpacing: 10
-            }, this.domNode);
-            var tr = domConstruct.create("tr", null, table);
-            var td = domConstruct.create("td", null, tr);
-            var label = domConstruct.create("label", {
-                for: this.id + "Name",
-                innerHTML: "Name:"
-            }, td);
-            tr = domConstruct.create("tr", null, table);
-            td = domConstruct.create("td", null, tr);
-            this.nameInput = new TextBox({
-                id: this.id + "Name",
-                type: "text",
-                name: "name",
-                trim: "true",
-                properCase: true
-            }).placeAt(td);
-            this.own(this.nameInput);
 
-            tr = domConstruct.create("tr", null, table);
-            domConstruct.create("td", null, tr);
-            tr = domConstruct.create("tr", null, table);
-            td = domConstruct.create("td", null, tr);
-            this.own(new Button({
-                label: "Greet",
-                onClick: function (e) {
-                    this.onChange(this.nameInput.get("value"));
-                }.bind(this)
-            }).placeAt(td));
-        }
-    });
+export default declare([_WidgetBase], {
+    onChange: function (e) {
+        return true;
+    },
+    buildRendering: function () {
+        this.domNode = domConstruct.create("div");
+        var table = domConstruct.create("table", {
+            style: {
+                border: "1px solid #9f9f9f"
+            },
+            cellSpacing: 10
+        }, this.domNode);
+        var tr = domConstruct.create("tr", null, table);
+        var td = domConstruct.create("td", null, tr);
+        var label = domConstruct.create("label", {
+            for: this.id + "Name",
+            innerHTML: "Name:"
+        }, td);
+        tr = domConstruct.create("tr", null, table);
+        td = domConstruct.create("td", null, tr);
+        this.nameInput = new TextBox({
+            id: this.id + "Name",
+            type: "text",
+            name: "name",
+            trim: "true",
+            properCase: true
+        }).placeAt(td);
+        this.own(this.nameInput);
 
+        tr = domConstruct.create("tr", null, table);
+        domConstruct.create("td", null, tr);
+        tr = domConstruct.create("tr", null, table);
+        td = domConstruct.create("td", null, tr);
+        this.own(new Button({
+            label: "Greet",
+            onClick: function (e) {
+                this.onChange(this.nameInput.get("value"));
+            }.bind(this)
+        }).placeAt(td));
+    }
+});
